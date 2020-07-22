35fa80d311e75e5cde0ddd78b85433080c0690a1
Kirill Vasiliev <vasiliev.k.v@gmail.com>
2020-07-22 17:36:35 +0300

    Base app

---
+++ app/src/main/java/ru/vasiliev/dontkillmyapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.vasiliev.dontkillmyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.vasiliev.dontkillmyapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/vasiliev/dontkillmyapp/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.vasiliev.dontkillmyapp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
